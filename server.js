bd0f16365bf025f2f61dd8431195dcda1ef8596c
Pedro Capriles <caprilespe@outlook.com>
2020-01-15 18:21:20 -0300

    Adding router and test de routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = express.Router();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
@@ -7,10 +8,18 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 
-app.use('/', function(request, response) {
-    response.send('I am okay ✅🤖');
+router.get('/message', function(request, response){
+    response.send('Lista de mensajes');
 });
+
+router.delete('/message', function(request, response){
+    response.send('Mensaje Añadido');
+})
+// app.use('/', function(request, response) {
+//     response.send('I am okay ✅🤖');
+// });
 
 app.listen(3000, ()=>{
     console.info('Express was init successfully. ✅🚀');
