cdd4d89110dcfdae4b78caf27bbfd58767711714
Pedro Capriles <caprilespe@outlook.com>
2020-01-22 18:55:33 -0300

    Fix mongo data base connection URL changed the drive version 2.2.12

--- src/components/message/store.js
+++ src/components/message/store.js
@@ -3,21 +3,21 @@ const Model = require("./model");
 require("dotenv").config({ path: "./db.env" });
 db.Promise = global.Promise;
 db.connect(
-  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority
+  `,
   { useNewUrlParser: true, useUnifiedTopology: true }
 ).then(()=>{
     console.info('[db] Connect Successfully. ✅🟢');
 });
-
-//mongodb+srv://root:<password>@cluster0-aabdq.azure.mongodb.net/test?retryWrites=true&w=majority
 
 function addMessage(message) {
   const myMessage = new Model(message);
   myMessage.save();
 }
 
-function getMessages() {
-  return list;
+async function getMessages() {
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
